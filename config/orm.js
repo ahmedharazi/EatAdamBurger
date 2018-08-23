e2c48635879c1809741c38b4fbb3c970a5d9476b
Adam Yab <ahmed.harazi22@gmail.com>
2018-08-22 21:40:19 -0400

    upload all the file

---
+++ config/orm.js
@@ -0,0 +1,29 @@
+var connection = require("../config/connection.js");
+
+
+var orm = {
+  // create conecction to 
+  all: function (tableInput, cb) {
+    connection.query("SELECT * FROM " + tableInput + ';', function (err, result) {
+      if (err) throw err;
+      cb(result);
+    })
+  },
+  update: function (tableInput, condition, cb) {
+    connection.query(
+      "UPDATE " + tableInput + " SET devoured=true WHERE id=" + condition + ";",
+      function (err, result) {
+        cb(result);
+      })
+  },
+  create: function (tableInput, val, cb) {
+    connection.query(
+      "INSERT INTO " + tableInput + " (burger_name) VALUES ('"+val+"');",
+      function (err, result) {
+        if (err) throw err;
+        cb(result);
+      })
+  }
+};
+
+module.exports = orm;
